19147dda5cd48f996ba59e1ea0eaff8d3af00300
Tom de Goede <tom.degoede@afas.nl>
2021-04-07 19:44:09 +0200

    More tests & suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,6 +7,8 @@ import Application
 import Execution
 import Control.Monad.Identity
 import Data.Map as Map
+import Test.Tasty
+import Test.Tasty.HUnit
 
 type ProductId     = Int
 type BankAccountId = Int
@@ -15,10 +17,11 @@ type Balance       = Int
 type Price         = Int
 
 type Name = String
+type Order = (ProductId, BankAccountId, Amount)
 
-newtype OrderPlacedEvent    = OrderPlacedEvent    (ProductId, BankAccountId, Amount) deriving (Show)
-newtype OrderSucceededEvent = OrderSucceededEvent (ProductId, BankAccountId, Amount) deriving (Show)
-newtype OrderFailedEvent    = OrderFailedEvent    (ProductId, BankAccountId, Amount) deriving (Show)
+newtype OrderPlacedEvent    = OrderPlacedEvent    Order deriving (Show)
+newtype OrderSucceededEvent = OrderSucceededEvent Order deriving (Show)
+newtype OrderFailedEvent    = OrderFailedEvent    Order deriving (Show)
 newtype ProductCreatedEvent = ProductCreatedEvent (ProductId, Name, Price) deriving (Show)
 newtype StockAddedEvent     = StockAddedEvent     (ProductId, Amount) deriving (Show)
 newtype BalanceAddedEvent   = BalanceAddedEvent   (BankAccountId, Balance) deriving (Show)
@@ -26,9 +29,10 @@ newtype BalanceAddedEvent   = BalanceAddedEvent   (BankAccountId, Balance) deriv
 -- TODO make lookups state native
 newtype BankAccounts = BankAccounts (Map.Map BankAccountId Balance) deriving (Show)
 newtype Products     = Products     (Map.Map ProductId (Name, Price, Amount)) deriving (Show)
+newtype Orders       = Orders       [(Order, Bool)] deriving (Show)
 
 type POCEvents = '[OrderPlacedEvent, OrderSucceededEvent, OrderFailedEvent, ProductCreatedEvent, StockAddedEvent, BalanceAddedEvent]
-type POCStates = '[BankAccounts, Products]
+type POCStates = '[BankAccounts, Products, Orders]
 
 balanceAddedHandler :: EventHandler '[BankAccounts] '[BankAccounts] BalanceAddedEvent '[] ()
 balanceAddedHandler = do
@@ -68,6 +72,18 @@ orderPlacedHandler = do
       raiseEvent $ OrderSucceededEvent o
     _ -> raiseEvent $ OrderFailedEvent o
 
+orderFailedHandler :: EventHandler '[Orders] '[Orders] OrderFailedEvent '[] ()
+orderFailedHandler = do
+  (OrderFailedEvent o) <- handle
+  (Orders os) <- readState
+  writeState $ Orders $ (o, False) : os
+
+orderSucceededHandler :: EventHandler '[Orders] '[Orders] OrderSucceededEvent '[] ()
+orderSucceededHandler = do
+  (OrderSucceededEvent o) <- handle
+  (Orders os) <- readState
+  writeState $ Orders $ (o, True) : os
+
 end :: HandlersT '[] POCEvents POCStates
 end = HNil
 
@@ -75,23 +91,48 @@ handlers = addHandler balanceAddedHandler
          $ addHandler stockAddedHandler
          $ addHandler productCreatedHandler
          $ addHandler orderPlacedHandler
+         $ addHandler orderFailedHandler
+         $ addHandler orderSucceededHandler
        end
 
-initialStates = TCons (Identity $ BankAccounts empty) (TCons (Identity $ Products empty) TNil)
+initialStates = TCons (Identity $ BankAccounts empty) 
+              $ TCons (Identity $ Products empty) 
+              $ TCons (Identity $ Orders []) 
+              TNil
 
 application = Application handlers (emptyTypeList []) (emptyTypeList [])
 
-pocEvents :: [Event POCEvents]
-pocEvents = [ event $ ProductCreatedEvent (1, "Fiets", 100)
-            , event $ StockAddedEvent (1, 1)
-            , event $ BalanceAddedEvent (1, 100)
-            , event $ OrderPlacedEvent (1, 1, 1)
-            ]
+pocEvents1 :: [Event POCEvents]
+pocEvents1 = [ event $ ProductCreatedEvent (1, "Fiets", 100)
+             , event $ StockAddedEvent (1, 1)
+             , event $ BalanceAddedEvent (1, 100)
+             , event $ OrderPlacedEvent (1, 1, 1)
+             ]
 
-runPoc :: IO ()
-runPoc = do
-  finalStates <- executeCompletely application pocEvents initialStates
+pocEvents2 :: [Event POCEvents]
+pocEvents2 = [ event $ ProductCreatedEvent (1, "Fiets", 110)
+             , event $ StockAddedEvent (1, 1)
+             , event $ BalanceAddedEvent (1, 100)
+             , event $ OrderPlacedEvent (1, 1, 1)
+             ]
+
+pocEvents3 :: [Event POCEvents]
+pocEvents3 = [ event $ ProductCreatedEvent (1, "Fiets", 100)
+             , event $ StockAddedEvent (1, 1)
+             , event $ BalanceAddedEvent (1, 100)
+             , event $ OrderPlacedEvent (1, 1, 2)
+             ]
+
+runPoc evs = do
+  finalStates <- executeCompletely application evs initialStates
   print finalStates
 
-main :: IO ()
-main = runPoc
+
+tests :: TestTree
+tests = testGroup "Implementations" 
+  [ testCase "Test 1" $ runPoc pocEvents1
+  , testCase "Test 2" $ runPoc pocEvents2
+  , testCase "Test 3" $ runPoc pocEvents3
+  ]
+
+main = defaultMain tests
